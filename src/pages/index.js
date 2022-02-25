57d330ba109cbafeb9fc1f192fbce82d83123af5
Marcus Little <marcuslittle@Marcuss-MacBook-Pro.local>
2022-02-25 13:55:20 -0500

    log jquery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
+import jQuery from "jquery";
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
@@ -22,6 +23,8 @@ const BlogIndex = ({ data, location }) => {
       </Layout>
     )
   }
+
+  console.log(jQuery);
 
   return (
     <Layout location={location} title={siteTitle}>
